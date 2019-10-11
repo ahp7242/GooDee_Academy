e612128652b8e837e1d0cfae78f1121e6d504996
GD3 <GD3@DESKTOP-U50CULR>
2019-10-11 18:06:23 +0900

    index페이지에 전체 테이블과 테이블들의 행의 수를 출력

---
+++ sakila/src/sakila/address/model/StoreDao.java
@@ -0,0 +1,32 @@
+package sakila.address.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import sakila.db.DBHelper;
+
+public class StoreDao {
+
+	// index 페이지에 테이블 행의수 출력시 사용
+	public int selectStoreCount() {
+		int count = 0;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT COUNT(*) FROM store";
+		try {
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt("COUNT(*)");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		return count;
+	}
+}
